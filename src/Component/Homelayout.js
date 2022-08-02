31891e902349b43e315c016bbdc4db834a505044
elise1914347 <eliseduse@gmail.com>
2022-08-02 08:23:41 +0200

    footerAndabout

--- src/Component/Homelayout.js
+++ src/Component/Homelayout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Nav from "../Component/Navbar/Navbar"
-// import Footer from "./footer";
+import Footer from "./Footer";
 const HomeLayout = ({children})=>{
     return(
         <>
         <Nav/>
         <div style={{ minHeight:"70vh"}}>{children}</div>
-        {/* <Footer/> */}
+        <Footer/>
         </>
     );
 };
