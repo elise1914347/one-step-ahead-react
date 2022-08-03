dc192401182a8ca2068706a102af80eb51a332c3
Inemajoy <94519587+Inemajoy@users.noreply.github.com>
2022-08-03 11:30:01 +0200

    creating getstarted forms

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,23 @@ import React from "react";
 
 import {Routes,Route} from 'react-router-dom';
 import AboutUs from "../view/aboutUs";
-import Home from "../view/Home"
+import Home from "../view/Home";
+import GetStarted from "../view/Getstarted";
+import CoupleForm from "../view/couple";
+import IndividualForm from "../view/individual";
+import DecideForm from "../view/decide";
 const index=() =>{
     return (
         <>
          
        
         <Routes>
-         <Route exact path="/Home" element={<Home/>}></Route>  
-            <Route exact path="/aboutus" element={<AboutUs/>}></Route>
+        <Route exact path="/Home" element={<Home/>}></Route>  
+        <Route exact path="/aboutus" element={<AboutUs/>}></Route>
+        <Route exact path="/start" element={<GetStarted/>}></Route>
+        <Route exact path="/couple" element={<CoupleForm/>}></Route>
+        <Route exact path="/individual" element={<IndividualForm/>}></Route>
+        <Route exact path="/decide" element={<DecideForm/>}></Route>
         </Routes>
     
       
---
+++ src/view/Getstarted.js
@@ -0,0 +1,50 @@
+import React from "react";
+import depression from"../asset/depression.webp";
+import stress from "../asset/stress.webp";
+import relationship from "../asset/relationship.webp";
+import {useNavigate} from "react-router-dom";
+import "./Getstarted.css";
+import {Form, Input, InputNumber,Card } from "antd";
+
+const GetStarted =()=>{
+  const navigate = useNavigate();
+
+  const navigateToForm1 = () => {
+    navigate('/couple');
+  };
+  const navigateToForm2 = () => {
+    navigate('/individual');
+  };
+  const navigateToForm3 = () => {
+    navigate('/decide');
+  };
+
+return(
+  <>
+
+<Card classname="card">
+<Form className="form">
+<Form.Item label="would you allow your partern to join?" className="label"></Form.Item>
+      <div className="input">
+      <Form.Item label="" >
+      <button className="inputB" onClick={navigateToForm1}>yes(couples Therapy)</button>
+      </Form.Item>
+      <Form.Item label="">
+      <button className="inputB" onClick={navigateToForm2}>no(Individual Therapy)</button>
+      </Form.Item>
+      <Form.Item label="">
+      <button className="inputB" onClick={navigateToForm3}>Not sure yet(decide later)</button>
+      </Form.Item>
+      </div>
+      <Form.Item label="we'll match you to the Therapist that can help you" className="label"></Form.Item>
+    </Form>
+</Card>
+<div className="image">
+  <img src={depression} className="image1"/>
+  <img src={stress} className="image1"/>
+  <img src={relationship} className="image1"/>
+</div>
+</>
+)
+}
+export default GetStarted;
