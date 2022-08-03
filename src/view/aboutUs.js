be5171a0d289bec21d51805c51f6abf7389e03c6
elise1914347 <eliseduse@gmail.com>
2022-08-03 09:14:09 +0200

    change

--- src/view/aboutUs.js
+++ src/view/aboutUs.js
@@ -1,86 +1,89 @@
 import React from "react";
 import HomeLayout from "../Component/Homelayout";
 import "../Component/Navbar/Navbar.css";
-// import photo from "../asset/gu.jpg";
-// import logo from "../asset/logo.jpg";
-import { Button, Card } from 'antd';
-import people from "../asset/yg.jpg";
-import { Carousel } from 'antd';
+import { Button, Card, Col, Row } from "antd";
 
+import Carousel from "react-bootstrap/Carousel";
+import Success from "../asset/Images/ryan-success.jpg";
+import Successe from "../asset/Images/ghita-success.jpg";
+import Successy from "../asset/Images/laura-success.jpg";
 
 const About = () => {
+  
   return (
     <>
-
-      <div className="images" style={{backgroundColor:"green"}}>
-        <div className="nav">
-        <ul className="nav-links" style={{marginTop:"397px",marginLeft:"50%",backgroundPosition:"center"}}>
-          <li>
-            <a href="/home" className="nav-link">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/aboutus" className="nav-link">
-              aboutUs
-            </a>
-          </li>
-          <li>
-            <a href="ContactUs" className="nav-link">
-              ContactUs
-            </a>
-          </li>
-          <li>
-            <a href="Review" className="nav-link">
-              Review
-            </a>
-          </li>
-          <li>
-            <a href="Review" className="nav-link">
-              LogIn
-            </a>
-          </li>
-          <li>
-            <a  href="Review" className="nav-link btn-link">
-              GetStarted
-            </a>
-          </li>
-        </ul>
-      </div>
-      </div>
+      <div className="images" style={{ backgroundColor: "green" }}></div>
       <section>
-      <p style={{color:"green",textAlign:"center"}}>our mission</p><br></br>
-        <p style={{textAlign:"center"}}>
-          Making professional therapy accessible, affordable and convenient so<br></br>
+        <p style={{ color: "green", textAlign: "center", fontSize: "20px" }}>
+          our mission
+        </p>
+        <br></br>
+        <p style={{ textAlign: "center" }}>
+          Making professional therapy accessible, affordable and convenient so
+          <br></br>
           anyone who trugles with life challenges can get help, anytime and
           anywhere.
-        </p> <br></br>
-        <p style={{color:"green",textAlign:"center",fontSize:"20px"}}>our Therapist</p><br></br>
-        <p style={{textAlign:"center"}}>
-          Making professional therapy accessible, affordable and convenient so<br></br>
+        </p>{" "}
+        <br></br>
+        <p style={{ color: "green", textAlign: "center", fontSize: "20px" }}>
+          our Therapist
+        </p>
+        <br></br>
+        <p style={{ textAlign: "center" }}>
+          Making professional therapy accessible, affordable and convenient so
+          <br></br>
           anyone who trugles with life challenges can get help, anytime and
           anywhere.
-        </p> <br></br>
+        </p>{" "}
+        <br></br>
       </section>
-      <div className="carausel-img">
-      <Carousel autoplay> 
-      <Card>
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img> 
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img>
-        <img src={people} alt="elize" ></img></Card>
-  </Carousel> 
-  </div> 
-  <Button style={{marginLeft:"45%",color:"green"}}>Meet Our Therapist</Button>
-  <p style={{color:"green",textAlign:"center",fontSize:"20px"}}>our Success stories</p>
-  
-     </>
+      <Button style={{ marginLeft: "45%", color: "green" }}>
+        Meet Our Therapist
+      </Button>
+      <p style={{ textAlign: "center" }}>
+        <br></br>
+        onestepAhead offers access to licensed, trained, experienced, and
+        accredited psychologists (PhD / PsyD), marriage and family therapists
+        <br></br>
+        (LMFT), clinical social workers (LCSW / LMSW), and board licensed
+        professional counselors (LPC).
+      </p>
+      <br></br>
+     
+      <Carousel  autoplay>
+        <Carousel.Item className="anyone">
+          <img
+            className="d-block w-100"
+            src="https://dy7glz37jgl0b.cloudfront.net/about/counselors/0e0ddf50996ae717b7a67eeb8f651325.jpg?v=966d54b722cb"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item className="anyone">
+          <img
+            className="d-block w-100"
+            src="https://dy7glz37jgl0b.cloudfront.net/about/counselors/0db4dae3eadbb824e1a72401ec594c1f.jpg?v=966d54b722cb"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item className="anyone">
+          <img
+            className="d-block w-100"
+            src="https://dy7glz37jgl0b.cloudfront.net/about/counselors/8c6830bb46988faf7ffe0d8facea38e4.jpg?v=966d54b722cb"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+      <p style={{ color: "green", textAlign: "center", fontSize: "20px" }}>
+        our Success stories
+      </p>
+      <div>
+        <img src={Success} className="Success" />
+        
+        <img src={Successe} className="Success" />
+        <img src={Successy} className="Success" />
+
+      </div>
+    </>
   );
 };
 export default About;
