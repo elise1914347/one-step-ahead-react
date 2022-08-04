eaf5801170fe96ae1210e87bab02c886df259726
elise1914347 <eliseduse@gmail.com>
2022-08-04 12:06:40 +0200

    loginAndAbout

--- src/view/aboutUs.js
+++ src/view/aboutUs.js
@@ -9,10 +9,9 @@ import Successe from "../asset/Images/ghita-success.jpg";
 import Successy from "../asset/Images/laura-success.jpg";
 
 const About = () => {
-  
   return (
     <>
-      <div className="images" style={{ backgroundColor: "green" }}></div>
+      <div className="images"></div>
       <section>
         <p style={{ color: "green", textAlign: "center", fontSize: "20px" }}>
           our mission
@@ -49,39 +48,65 @@ const About = () => {
         professional counselors (LPC).
       </p>
       <br></br>
-     
-      <Carousel  autoplay>
+
+      <Carousel autoplay>
         <Carousel.Item className="anyone">
           <img
             className="d-block w-100"
-            src="https://dy7glz37jgl0b.cloudfront.net/about/counselors/0e0ddf50996ae717b7a67eeb8f651325.jpg?v=966d54b722cb"
+            src="https://images.pexels.com/photos/5699424/pexels-photo-5699424.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item className="anyone">
           <img
             className="d-block w-100"
-            src="https://dy7glz37jgl0b.cloudfront.net/about/counselors/0db4dae3eadbb824e1a72401ec594c1f.jpg?v=966d54b722cb"
+            src="https://images.pexels.com/photos/5699424/pexels-photo-5699424.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="Second slide"
           />
         </Carousel.Item>
         <Carousel.Item className="anyone">
           <img
             className="d-block w-100"
-            src="https://dy7glz37jgl0b.cloudfront.net/about/counselors/8c6830bb46988faf7ffe0d8facea38e4.jpg?v=966d54b722cb"
+            src="https://images.pexels.com/photos/5699424/pexels-photo-5699424.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="Third slide"
           />
         </Carousel.Item>
       </Carousel>
+
       <p style={{ color: "green", textAlign: "center", fontSize: "20px" }}>
         our Success stories
       </p>
-      <div>
+      <div className="our">
         <img src={Success} className="Success" />
-        
         <img src={Successe} className="Success" />
         <img src={Successy} className="Success" />
-
+        </div>
+        {/* <br></br><br></br> */}
+    <div className="allworld">
+      <h1 className="allworld">
+        "The biggest achievement I've made is, truly, fighting for myself.
+        <br></br>
+        Through working with Aiko and taking the time to discuss various issues
+        <br></br>
+        with her, I've become a bigger advocate for myself, and I'm so proud of
+        <br></br>
+        that."
+      </h1>
+      <Button style={{fontSize:"10px",textAlign:"center",float:"left",marginTop:"50px",background:"green"}} >ReadMore</Button>
+      <h2 className="allworld">
+        "I have now been working with Sheilah on BetterHelp for about 4 weeks<br></br>
+        and can confidently say that I am a lot less anxious about my career
+        <br></br>
+        path now than when I first signed up...I am so much better off now than
+        <br></br>I was before."
+      </h2>
+      <Button style={{fontSize:"10px",textAlign:"center",float:"left",marginTop:"50px",background:"green"}}>ReadMore</Button>
+      <h3 className="allworld">
+        "I am a totally different person now than when I signed up 19 months<br></br>
+        ago. I have found my voice and can speak up for myself now....My<br></br>
+        confidence level is also better than it ever has been."
+      </h3>
+      <Button style={{fontSize:"10px",textAlign:"center",float:"right",marginTop:"50px",background:"green"}}>ReadMore</Button>
       </div>
     </>
   );
