1472f080b922bce19b6c3b09d0bec8a227583bcf
elise1914347 <eliseduse@gmail.com>
2022-08-23 11:49:00 +0200

    Onother

--- src/view/login.js
+++ src/view/login.js
@@ -6,6 +6,7 @@ import Loginphto from "../asset/Images/undraw_Mobile_login_re_9ntv.png";
 import Getimage from "../asset/profilepic.webp";
 import Getimage2 from "../asset/profilepic_2.jpg";
 import Lge from "../asset/Images/loginPage.png.png";
+import SignUp from "../view/sign-upget";
 
 
 const Login= () => {
@@ -55,71 +56,15 @@ const Login= () => {
           Forgot password
         </a>
       </Form.Item>
-
       <Form.Item>
+        <a href="/signup" style={{color:"white", fontSize:"20px",marginLeft:"60%",background:"black"}}>SignUp</a>
         <Button type="primary" htmlType="submit" className="login-form-button" style={{marginLeft:"1px",width:"100%"}}>
           Log in
         </Button>
-        {/* Or <a href="">register now!</a> */}
       </Form.Item>
-      {/* <img src={Loginphto} alt="" className="logn"></img> */}
       </Card>
-      {/* <Card className="yegiiiiry" style={{backgroundColor:"rgb(35, 35, 133)",borderRadius:"60px",marginLeft:"9rem",paddingBottom:"30px"}}>
-      <h1 style={{color:"white",fontSize:"20px"}}>"Start by doing what's necessary; <br></br>
-        then do what's possible; <br></br>and suddenly you are doing the impossible."<br></br></h1>
-    </Card> */}
     </div>
-    
-
     </div>
-    {/* <div className="lginra">
-    <Carousel autoplay style={{ marginLeft: "35%", marginRight: "25%" }} >
-          <div>
-            <div style={{ display: "flex", gap: "5rem" }}>
-              <img
-                src={Getimage}
-                style={{ borderRadius: "100%", height: "6rem", width: "6rem" }}
-              />
-              <div >
-                <h5 style={{color:"black"}}>Latest Review For</h5>
-                <h4 style={{ color: "yellow" }}>DUSENGE FABIOLA</h4>
-              </div>
-            </div>
-            <div style={{marginTop:"2rem"}}>
-              <p style={{ marginLeft: "2rem",color:"black" }}>N.I. wrote 2 days ago</p>
-              <p style={{ fontSize: "15px",color:"black"}}>
-                My experience have been fulfilling and healing
-                <br />I like Jessica she is my therapist but i feel like she is
-                my best friend
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div style={{ display: "flex", gap: "5rem" }}>
-              <img
-                src={Getimage2}
-                style={{ borderRadius: "100%", height: "6rem", width: "6rem" }}
-              />
-              <div >
-                <h5 style={{color:"black"}}>Latest Review For</h5>
-                <h3 style={{ color: "yellow" }}>Hailla Jack</h3>
-              </div>
-            </div>
-            <div style={{marginTop:"2rem"}}>
-              <p style={{ marginLeft: "2rem",color:"black"}}>H.J. wrote 3 days ago</p>
-              <p style={{ fontSize: "15px",color:"black"}}>
-                My experience have been fulfilling and healing
-                <br />I like patric! he has helped me in different aspect of my
-                healing and my journey. he is my therapist but i feel like she is my best
-                friend
-              </p>
-            </div>
-          </div>
-        </Carousel>
-        </div> */}
-     
-   
     </>
   );
 };
